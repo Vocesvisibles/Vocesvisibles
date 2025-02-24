e252b64832f5e6e3f9c1689b1801663800bf641f
Vocesvisibles <benjymarrcadiz@gmail.com>
2025-02-24 18:06:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add interactive elements if needed later. 
+// For now, it's just a placeholder for any future functionalities.
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to Voces Visibles website!");
+});
